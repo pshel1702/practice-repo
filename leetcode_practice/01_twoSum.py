07f656880d84086494bf3d187e1fa528417458ac
Prachi Shelgikar <praachi.shelgikar@gmail.com>
2021-10-05 00:24:04 +0000

    Added logic for twoSum

--- leetcode_practice/01_twoSum.py
+++ leetcode_practice/01_twoSum.py
@@ -8,11 +8,26 @@ class twoSumTest(unittest.TestCase):
         target = 3
         result = twoSum(nums,target)
         self.assertEqual(result, [0,1])
+    
+    def test_four_numbers(self):
+        nums = [2,7,11,15]
+        target = 9
+        result = twoSum(nums,target)
+        self.assertEqual(result,[0,1])   
+
+    def test_three_numbers(self):
+        nums = [3,2,4]
+        target = 6
+        result = twoSum(nums,target)
+        self.assertEqual(result,[1,2])  
 
 
 def twoSum(nums,target):
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i]+nums[j]==target:
+                return [i,j]
     
-    return [0,1]
 
 if __name__=="__main__":
     unittest.main()
