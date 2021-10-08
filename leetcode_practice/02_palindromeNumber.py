08e84469fa9acd19ced8065d23e2c2f0ec8b5891
Prachi Shelgikar <praachi.shelgikar@gmail.com>
2021-10-08 00:08:39 +0000

    Added tests for multiple digits, and negative integer

--- leetcode_practice/02_palindromeNumber.py
+++ leetcode_practice/02_palindromeNumber.py
@@ -5,16 +5,37 @@ class palindromeNumberTest(unittest.TestCase):
     def test_singleDigit(self):
         num = 1
         result = isPalindrome(num)
-        self.assertEqual(result,True)
+        self.assertTrue(result)
 
     def test_twoDifferentDigits(self):
         num = 23
         result = isPalindrome(num)
-        self.assertEqual(result, False)
+        self.assertFalse(result)
+    
+    def test_threePalindromeDigits(self):
+        num = 121
+        result = isPalindrome(num)
+        self.assertTrue(result)
+    
+    def test_fivePalindromeDigits(self):
+        num = 12321
+        result = isPalindrome(num)
+        self.assertTrue(result)
+
+    def test_fiveNonPalindromeDigits(self):
+        num = 12231
+        result = isPalindrome(num)
+        self.assertFalse(result)
+
+    def test_negativeNonPalindrome(self):
+        num = -131
+        result = isPalindrome(num)
+        self.assertFalse(result)
 
 
 def isPalindrome(num):
     num_as_str = str(num)
+    #return num_as_str[0] == num_as_str[-1]
     return num_as_str == num_as_str[::-1]
 
 
