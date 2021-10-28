c7a21150ceaff4a864d129b1e285c486bd1adc7a
Prachi Shelgikar <praachi.shelgikar@gmail.com>
2021-10-28 23:50:39 +0000

    Added logic for get_parent to pass for node at second level

--- leetcode_practice/03_binaryTreeCousins.py
+++ leetcode_practice/03_binaryTreeCousins.py
@@ -22,10 +22,26 @@ class BinaryTreeCousinsUnitTests(unittest.TestCase):
         node=3
         result = get_parent(root,node)
         self.assertEqual(result,1) 
-
+    
+    def test_get_parent_child_at_second_level(self):
+        root = [1,2,3,4]
+        node = 4
+        result = get_parent(root,node)
+        self.assertEqual(result,2)
 
 def get_parent(root,node):
-    return 1
+    #Pseudocode
+    #iterate over root to find node
+    #if index of node is even, parent == root[i-3], else parent == root[i-2]
+    node_index = root.index(node)
+    #add function to check index
+    if(node_index==1) or (node_index==2):
+        return root[0]
+
+    if(node_index%2 == 0):
+        return root[node_index-3]
+    return root[node_index-2]
+
     
 def are_cousins(root,x,y):
     ##Pseudocode
