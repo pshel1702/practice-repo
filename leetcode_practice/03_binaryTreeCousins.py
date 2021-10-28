d623ce5abe661bfbe9cfa9a8fc66d1cacb9d2528
Prachi Shelgikar <praachi.shelgikar@gmail.com>
2021-10-28 23:33:37 +0000

    Added unit tests to get parent for root with one child and 2 children

--- leetcode_practice/03_binaryTreeCousins.py
+++ leetcode_practice/03_binaryTreeCousins.py
@@ -9,8 +9,32 @@ class BinaryTreeCousinsAcceptanceTests(unittest.TestCase):
         result = are_cousins(root,x,y)
         self.assertFalse(result)
 
+class BinaryTreeCousinsUnitTests(unittest.TestCase):
+
+    def test_get_parent_root_with_one_child(self): #start basic and build
+        root = [1,2]
+        node=2
+        result = get_parent(root,node)
+        self.assertEqual(result,1)
+    
+    def test_get_parent_root_with_two_children(self): 
+        root = [1,2,3]
+        node=3
+        result = get_parent(root,node)
+        self.assertEqual(result,1) 
+
+
+def get_parent(root,node):
+    return 1
+    
 def are_cousins(root,x,y):
+    ##Pseudocode
+    #Find parent and depth of x in root
+    #Repeat above step for y in root
+    #Return True if parent_x != parent_y and depth_x == depth_y
+    #Else return False
     raise
+
 
 
 if __name__ == "__main__":
