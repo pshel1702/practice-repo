6728c4dcd74a344a71b8e630b1f038e1cee349e6
Jason Nguyen <jason-nguyen@pluralsight.com>
2021-10-31 19:42:41 -0600

    added some behavioral tests for nodes at level 3

--- leetcode_practice/03_binaryTreeCousins.py
+++ leetcode_practice/03_binaryTreeCousins.py
@@ -24,6 +24,20 @@ class BinaryTreeCousinsAcceptanceTests(unittest.TestCase):
         result = are_cousins(root, x, y)
         self.assertFalse(result)
 
+    def test_are_cousins_false_when_siblings_at_third_level(self):
+        root = [1, 2, 3, None, 4, None, 5, 6, 7, 8, 9]
+        x = 6
+        y = 7
+        result = are_cousins(root, x, y)
+        self.assertFalse(result)
+
+    def test_are_cousins_true_when_dif_parent_same_depth_at_third_level(self):
+        root = [1, 2, 3, None, 4, None, 5, 6, 7, 8, 9]
+        x = 7
+        y = 8
+        result = are_cousins(root, x, y)
+        self.assertTrue(result)
+
 
 class BinaryTreeCousinsUnitTests(unittest.TestCase):
 
