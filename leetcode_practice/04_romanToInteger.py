2ad7101ab63975979b46b2ffcdd2ebbd8381e75a
Prachi Shelgikar <praachi.shelgikar@gmail.com>
2021-12-08 01:07:23 +0000

    Refactored code to use ddt module

--- leetcode_practice/04_romanToInteger.py
+++ leetcode_practice/04_romanToInteger.py
@@ -1,15 +1,30 @@
 import unittest
 
+from ddt import ddt, data, unpack
 
+
+@ddt
 class romanToIntegerConversion(unittest.TestCase):
-    def test_singleRomanInput(self):
-        s = 'I'
+    @data(('I', 1), ('V', 5), ('X', 10), ('L', 50), ('C', 100), ('D', 500), ('M', 1000))
+    @unpack
+    def test_standard_roman_symbols(self, s, expected):
         result = romanToInt(s)
-        self.assertEqual(result, 1)
+        self.assertEqual(result, expected)
+
+
+roman_to_integer_dict = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+}
 
 
 def romanToInt(s):
-    return 1
+    return roman_to_integer_dict[s]
 
 
 if __name__ == "__main__":
