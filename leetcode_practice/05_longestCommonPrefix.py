276d97c2c0403f48e5884648cfa374e8cc6f3192
Prachi Shelgikar <praachi.shelgikar@gmail.com>
2022-01-05 00:45:30 +0000

    Added functionality for multiple words with single letter common prefix

--- leetcode_practice/05_longestCommonPrefix.py
+++ leetcode_practice/05_longestCommonPrefix.py
@@ -12,11 +12,21 @@ class longestCommonPrefixTests(unittest.TestCase):
         result = longestCommonPrefix(strs)
         self.assertEqual(result, "")
 
+    def test_multiple_words_with_single_letter_common_prefix(self):
+        strs = ["for", "fire", "fan"]
+        result = longestCommonPrefix(strs)
+        self.assertEqual(result, "f")
+
 
 def longestCommonPrefix(strs):
     if len(strs) == 1:
         return "flower"
-    return ""
+    prefix = strs[0][0]
+    for _, item in enumerate(strs):
+        if item[0] == prefix:
+            continue
+        return ""
+    return prefix
 
 
 if __name__ == "__main__":
